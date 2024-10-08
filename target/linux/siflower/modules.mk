aa37f26ef419cbc275ff1f05cc37d067a6c81521
Chuanhong Guo <gch981213@gmail.com>
2024-10-08 14:18:27 +0800

    siflower: sf21a6826: new subtarget
    Siflower SF21A6826/SF21H8898 are a family of RISC-V SoCs with:
    
     * Quad-core T-Head C908 (1.125G for SF21A6826, 1.25G for SF21H8898)
     * DDR3/DDR4 memory controller
     * 1 QSGMII 4x1G
     * 1 SGMII/2500Base-X 2.5G
     * 1 additional RGMII on SF21H8898
     * Network offloading engine for L2 switching and L3 NAT
     * 2 PCIE Gen2 lanes, operating in either one PCIE Gen2x2 or two
       PCIE Gen2x1 mode
     * 1 USB2.0
    
    The board added in the final PR isn't ready yet. The two boards added
    in this PR are the EVB and a not-yet-ready gateway board
    
    TODO:
    * update kconfig for sf19a28
    * ethernet driver from siflower networking team
    
    Signed-off-by: Chuanhong Guo <gch981213@gmail.com>

--- target/linux/siflower/modules.mk
+++ target/linux/siflower/modules.mk
@@ -13,3 +13,19 @@ define KernelPackage/phy-sf19a2890-usb/description
 endef
 
 $(eval $(call KernelPackage,phy-sf19a2890-usb))
+
+define KernelPackage/phy-sf21a6826-usb
+  TITLE:=Siflower SF21A6826 USB 2.0 PHY Driver
+  KCONFIG:=CONFIG_PHY_SF21A6826_USB
+  DEPENDS:=@TARGET_siflower_sf21a6826
+  SUBMENU:=$(USB_MENU)
+  FILES:=$(LINUX_DIR)/drivers/phy/siflower/phy-sf21a6826-usb.ko
+  AUTOLOAD:=$(call AutoLoad,45,phy-sf21a6826-usb,1)
+endef
+
+define KernelPackage/phy-sf21a6826-usb/description
+  Support for Siflower SF21A6826 USB 2.0 PHY connected to the USB
+  controller.
+endef
+
+$(eval $(call KernelPackage,phy-sf21a6826-usb))
